f40e920fa51ee072956a1f2983e9ac7b42f6ec8d
bfw910 <bfw0910@gmail.com>
2026-06-03 11:04:43 +0700

    adding dialect

--- config/db.config.js
+++ config/db.config.js
@@ -1,7 +1,9 @@
 import { Sequelize } from "sequelize";
+import mysql from 'mysql2';
 const db =new Sequelize('web_lanjut','root','',{
     host:'localhost',
     dialect:'mysql',
+    dialectModule: mysql,
     define:{
          timestamps:false
     }
