ef3d4106f093a484432d95995e27c1610d596ffb
bfw910 <bfw0910@gmail.com>
2026-06-03 11:32:28 +0700

    a

--- config/db.config.js
+++ config/db.config.js
@@ -1,15 +1,21 @@
 import { Sequelize } from "sequelize";
-import mysql from 'mysql2';
-const db =new Sequelize('web_lanjut','root','',{
-    host:'localhost',
-    dialect:'mysql',
-    dialectModule: mysql,
-    define:{
-         timestamps:false
-    }
+import mysql from "mysql2";
+const db = new Sequelize("web_lanjut", "avnadmin", "AVNS_zxJHdAMA1aoa5qBhCtK", {
+  host: "mysql-153b5fd7-bfw0910-57c2.h.aivencloud.com",
+  dialect: "mysql",
+  port: 23126,
+  dialectOptions: {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
+  define: {
+    timestamps: false,
+  },
 });
+
 export default db;
 
-// (async()=>{
-//     await db.sync();
-// })();
+/*(async()=>{ 
+await db.sync({ alter: true }); 
+})();*/
